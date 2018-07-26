5f3252eaefbeaf5ecb449ff0bf83a0979fd4e683
monkeyChen <livepower@gmail.com>
2018-07-26 13:47:12 +0800

    add node

--- hex school/node/app.js
+++ hex school/node/app.js
@@ -1,6 +1,2 @@
-var http =  require('http');
-http.createServer(function(require,response){
-    response.writeHead(200,{"content-Type":"text-html"});
-    response.write("<h1>hello</h1>");
-    response.end();
-}).listen(8080);
+var test = 1;
+console.log(test);
