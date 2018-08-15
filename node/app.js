31a7bf073de3b16e32c6adaf0596df94940932dc
monkeyChen <livepower@gmail.com>
2018-08-15 15:29:58 +0800

    add csurf&flash feature

--- node/app.js
+++ node/app.js
@@ -4,21 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var session = require('express-session')
+var session = require('express-session');
+var csrf = require('csurf');
+var flash = require('connect-flash');
+require('dotenv').config();
+
+// routers list
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sessionRouter = require('./routes/session');
 var toDoListRouter = require('./routes/toDoList');
 var emailRouter = require('./routes/email');
-
-// load firebase database 
-// var admin = require("firebase-admin");
-// var serviceAccount = require("./project-0815-firebase-adminsdk-p86o9-f8cd831aaf.json");
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://project-0815.firebaseio.com"
-// });
-// _______________________________
 
 var app = express();
 
@@ -26,6 +22,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+// middleware function
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,7 +34,11 @@ app.use(session({
   secret: 'keyboard cat',
   resave: true,
   saveUninitialized: true
-}))
+}));
+app.use(csrf({ cookie: true }));
+app.use(flash());
+
+// page list
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/session', sessionRouter);
--- node/routes/email.js
+++ node/routes/email.js
@@ -3,9 +3,15 @@ var router = express.Router();
 var mail = require('nodemailer');
 
 
+
 /* GET email page. */
 router.get('/', function(req, res, next) {
-  res.render('email');
+  res.render('email',{ 
+    csrfToken: req.csrfToken() ,
+    nameNull: req.flash('name_info'),
+    emailNull: req.flash('email_info'),
+    subjectNull: req.flash('subject_info')
+  });
 });
 
 router.get('/review',(req,res)=>{
@@ -13,13 +19,28 @@ router.get('/review',(req,res)=>{
 });
 
 router.post('/post',(req,res)=>{
+  if(req.body.name == ''){
+    req.flash('name_info','姓名不可為空值');
+    res.redirect('/email');
+    return;
+  }else if(req.body.email == ''){
+    req.flash('email_info','信箱不可為空值');
+    res.redirect('/email');
+    return;
+  }else if(req.body.subject == ''){
+    req.flash('subject_info','標題不可為空值');
+    res.redirect('/email');
+    return;
+  }
+  
   var transporter = mail.createTransport({
     service : 'Gmail',
     auth:{
-      user:'livepower0815@gmail.com',
-      pass:'123427374'
+      user: process.env.gmail_user,
+      pass: process.env.gmail_pass
     }
   });
+  
   var mailOptions = {
     from: '"陳宏宇"<livepower0815@gmail.com>',
     to: 'livepower0815@gmail.com',
